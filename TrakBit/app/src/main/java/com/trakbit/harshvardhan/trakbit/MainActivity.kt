a7902668624985b02ba73aa9d191ad0bce1010ca
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-01-16 16:09:44 +0000

    navigation drawer

--- TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/MainActivity.kt
+++ TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/MainActivity.kt
@@ -2,11 +2,16 @@ package com.trakbit.harshvardhan.trakbit
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var drawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,22 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        initNavigationDrawer()
+    }
+
+    private fun initNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer)
+        val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            override fun onDrawerClosed(v: View?) {
+                super.onDrawerClosed(v)
+            }
+
+            override fun onDrawerOpened(v: View?) {
+                super.onDrawerOpened(v)
+            }
+        }
+        drawerLayout!!.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 
 }
