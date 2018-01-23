94997182ba67a6ff9dd7a7768b59f2ae707d1487
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-01-23 18:00:53 +0000

    clock button eventListener

--- TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/fragments/ClockFragment.kt
+++ TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/fragments/ClockFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.trakbit.harshvardhan.trakbit.R
+import kotlinx.android.synthetic.main.clock_fragment.*
 
 /**
  * Created by harshvardhan on 19/01/2018.
@@ -19,4 +20,11 @@ class ClockFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater!!.inflate(R.layout.clock_fragment,container,false)
     }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clockButton.setOnClickListener { println("clockButton")}
+    }
+
+
 }
