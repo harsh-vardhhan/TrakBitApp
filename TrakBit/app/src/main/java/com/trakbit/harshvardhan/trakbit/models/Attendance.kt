46b78d3d880bbe0d4cae7cc18ea5f3265061b916
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-03-06 10:07:10 +0000

    remove unused import

--- TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/models/Attendance.kt
+++ TrakBit/app/src/main/java/com/trakbit/harshvardhan/trakbit/models/Attendance.kt
@@ -1,7 +1,6 @@
 package com.trakbit.harshvardhan.trakbit.models
 
 import io.realm.RealmObject
-import org.joda.time.DateTime
 
 open class Attendance : RealmObject() {
     var clocking: String? = null
